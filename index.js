c84ad1da6614087cf65d3a2e996174b944a5f3b5
LukitaChan <kamery_94@hotmail.com>
2022-04-30 17:31:39 -0500

    getMNore500_Done

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ app.get('/FullList', (request, response) => {
 app.get('/HaveCertification', (request, response) => {
 	const listaCertificado = request.params.haveCertification;
 	const listaConCertificado = ExplorerController.getExplorersCertification(listaCertificado);
-	response.json({ Certificado: request.params.haveCertification, ExplorersConCertificado: listaConCertificado });
+	response.json({ Certificado: request.params.haveCertification, EstudiantesConCertificado: listaConCertificado });
+});
+
+app.get('/credits500', (request, response) => {
+	const lista500 = request.params.credits;
+	const listaMayor500 = ExplorerController.getExplorers500(lista500);
+	response.json({ Credits: request.params.credits, EstudiantesQuinientos: listaMayor500 });
 });
 
 app.listen(port, () => {
