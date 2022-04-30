b0b00557f7a07acb5f98acd0ab1731444820bb47
LukitaChan <kamery_94@hotmail.com>
2022-04-30 17:58:30 -0500

    bootstrap

--- router/RutasWeb.js
+++ router/RutasWeb.js
@@ -2,13 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	// define la route. En este caso por medio de EJS. (express.js).
 	res.render('index.ejs', { titulo: 'Code Challenge' });
-});
-
-router.get('/LukitaChan', (req, res) => {
-	//define la route (express.js)
-	res.render('index.ejs', { titulo: 'Hola a Todos!' });
 });
 
 module.exports = router;
